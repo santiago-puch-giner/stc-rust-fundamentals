03c5aae25017dd32387f4885a489fd454d8d871b
Santi Puch Giner <santi@cala.ai>
2026-08-20 22:01:49 +0200

    done

--- main.rs
+++ main.rs
@@ -2,9 +2,7 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
-    let mut iter = stdin.lock().lines();
-    let w: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
-    let h: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
-    // Print w * h.
-    println!("{}", w * h);
+    let line = stdin.lock().lines().next().unwrap().unwrap();
+    // Print the uppercase version.
+    println!("{}", line.to_uppercase());
 }
