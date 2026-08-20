7b4d66243bbbce5d3e48c1b2d389ebbb74da8961
Santi Puch Giner <santi@cala.ai>
2026-08-20 22:00:15 +0200

    done

--- main.rs
+++ main.rs
@@ -3,8 +3,8 @@ use std::io::{self, BufRead};
 fn main() {
     let stdin = io::stdin();
     let mut iter = stdin.lock().lines();
-    let a: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
-    let b: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
-    // Print a + b.
-    println!("{}", a + b);
+    let w: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    let h: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    // Print w * h.
+    println!("{}", w * h);
 }
