0d87360fcf29f9b6f27b9b77f14efdfa5b8f7aef
Santi Puch Giner <santi@cala.ai>
2026-08-20 21:57:35 +0200

    done

--- main.rs
+++ main.rs
@@ -1,4 +1,10 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    // Print the greeting below.
-    println!("Hello, Rust!");
+    let stdin = io::stdin();
+    let mut iter = stdin.lock().lines();
+    let a: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    let b: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    // Print a + b.
+    println!("{}", a + b);
 }
